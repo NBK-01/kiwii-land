4c10803df941fe2904b386e7cece54359e7c35d1
NBK-01 <nayefkanaan03@gmail.com>
2023-08-28 13:00:52 +0300

    Added toasts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+"use client"
 import { Button } from '@/components/ui/button'
+import {Toaster, toast} from "sonner"
 import Image from 'next/image'
 
 export default function Home() {
   return (
   <>
-
+  <Toaster/>
   <h1 className="text-6xl"> Hello </h1>
-  <Button> Test </Button>
+  <Button onClick = {() => toast.success("Done Deal")}> Test </Button>
 
   </>
        )
