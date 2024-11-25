bdfaf2ec9730c76580247f2426171704d58a6afc
NBK-01 <nayefkanaan03@gmail.com>
2024-11-25 12:17:00 +0200

    met data test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,40 @@
-import Navbar from '@/components/shared/nav'
-import './globals.css'
-import type { Metadata } from 'next'
-import { Analytics } from '@vercel/analytics/react';
-
-
+import Navbar from "@/components/shared/nav";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
-  title: 'Kiwii',
-  description: 'Do not miss out on the big reveal! Something exciting is coming your way',
-}
+  title: "Kiwii",
+  description:
+    "Do not miss out on the big reveal! Something exciting is coming your way",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <title>Kiwii</title>
+      <meta
+        name="description"
+        content="Do not miss out on the big reveal! Something exciting is coming your way"
+      />
+
+      <meta property="og:url" content="https://kiwii.app" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Kiwii" />
+      <meta
+        property="og:description"
+        content="Do not miss out on the big reveal! Something exciting is coming your way"
+      />
+      <meta property="og:image" content="/og-img.png" />
+
       <body className="">
         {children}
-        <Analytics/>
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
